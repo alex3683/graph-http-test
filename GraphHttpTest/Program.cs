0b7c7c6ee80b9f159da88bccd843a755796671bd
Alexander Wilden <alexander.wilden@timeflex.de>
2022-03-24 10:29:52 +0100

    Add project files.

---
+++ GraphHttpTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace GraphHttpTest
+{
+    internal class Program
+    {
+
+        private static readonly string TenantId = "";
+        private static readonly string ClientId = "";
+        private static readonly string ClientSecret = "";
+
+        private static void Main(string[] args)
+        {
+            var credentials = new ClientSecretCredential(TenantId, ClientId, ClientSecret);
+            var client = new GraphServiceClient(credentials);
+            var request = client.Domains
+                .Request()
+                .GetHttpRequestMessage();
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", GetCredentials().AccessToken);
+            //request.Version = new Version(1, 1);
+
+            var response = new HttpClient().SendAsync(request).Result;
+            response.EnsureSuccessStatusCode();
+            var content = response.Content.ReadAsStringAsync().Result;
+            Debug.WriteLine($"Status {response.StatusCode}: {content}");
+        }
+
+        private static AuthenticationResult GetCredentials()
+        {
+            return ConfidentialClientApplicationBuilder
+                .Create(ClientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, TenantId)
+                .WithClientSecret(ClientSecret)
+                .Build()
+                .AcquireTokenForClient(new string[] { "https://graph.microsoft.com/.default" })
+                .ExecuteAsync().Result;
+        }
+    }
+}
